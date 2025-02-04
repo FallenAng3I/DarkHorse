c4463c08f16d0b612e53f597ef6f4cb329bdd2e7
keeeksik <nanimak7@gmail.com>
2025-02-04 22:55:29 +0300

    init

--- Cursovaya/Assets/_Source/BarsSystem/HealthBar.cs
+++ Cursovaya/Assets/_Source/BarsSystem/HealthBar.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using PlayerSystem;
 using UnityEngine;
-
-public class HealthBar : MonoBehaviour
+using UnityEngine.UI;
+namespace BarsSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HealthBar : MonoBehaviour
     {
-        
-    }
+        public Image healthBar;
+        public Image staminaBar;
+        public Text healthText;
+        public Text staminaText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Player playerStats;
+
+        public void Initialize(Player stats)
+        {
+            playerStats = stats;
+            UpdateHealthBar();
+            UpdateStaminaBar();
+        }
+
+        public void UpdateHealthBar()
+        {
+            //healthBar.fillAmount = playerStats.currentHealth / playerStats.maxHealth;
+           // healthText.text = $"{playerStats.currentHealth}/{playerStats.maxHealth}";
+        }
+
+        public void UpdateStaminaBar()
+        {
+           // staminaBar.fillAmount = playerStats.currentStamina / playerStats.maxStamina;
+           // staminaText.text = $"{playerStats.currentStamina}/{playerStats.maxStamina}";
+        }
     }
-}
